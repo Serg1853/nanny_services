4d4879be1e4d1634315c4e992330d5e3595af98c
Serg1853 <sergey24226@gmail.com>
2024-03-26 17:35:36 +0200

    fix gh-pages

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,8 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/react_vite">
+    <BrowserRouter basename="/nanny_services">
       <App />
     </BrowserRouter>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
